5257c9077770bb89071fa16d75fab7f71a061f45
Werfmon <d.vyroubal.w@gmail.com>
2022-09-28 21:49:30 +0200

    first remote commit

--- src/main/java/cz/domin/chatappv2/Repository/FriendshipRepository.java
+++ src/main/java/cz/domin/chatappv2/Repository/FriendshipRepository.java
@@ -2,13 +2,16 @@ package cz.domin.chatappv2.Repository;
 
 
 import cz.domin.chatappv2.Model.Friendship;
+import cz.domin.chatappv2.Model.FriendshipStatus;
 import cz.domin.chatappv2.Model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, String> {
     Boolean existsFriendshipByMainPersonAndPerson(Person mainPerson, Person person);
     Optional<Friendship> findFriendshipByMainPersonAndPerson(Person mainPerson, Person person);
+    List<Friendship> findFriendshipsByMainPersonOrPersonAndStatusNot(Person mainPerson, Person person, FriendshipStatus friendshipStatus);
 }
--- src/main/java/cz/domin/chatappv2/Service/PersonService.java
+++ src/main/java/cz/domin/chatappv2/Service/PersonService.java
@@ -3,28 +3,21 @@ package cz.domin.chatappv2.Service;
 import cz.domin.chatappv2.Controller.dto.create.NewPersonDTO;
 import cz.domin.chatappv2.Helper.Convertor.Base64ImageConvertor;
 import cz.domin.chatappv2.Helper.Convertor.Base64ImageConvertorResponse;
-import cz.domin.chatappv2.Helper.Response.Response;
 import cz.domin.chatappv2.Helper.Response.ServiceResponse;
 import cz.domin.chatappv2.Model.Friendship;
+import cz.domin.chatappv2.Model.FriendshipStatus;
 import cz.domin.chatappv2.Model.Person;
 import cz.domin.chatappv2.Model.VisibilityStatus;
+import cz.domin.chatappv2.Repository.FriendshipRepository;
+import cz.domin.chatappv2.Repository.FriendshipStatusRepository;
 import cz.domin.chatappv2.Repository.PersonRepository;
-import cz.domin.chatappv2.Repository.VisibilityStatusRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,6 +29,8 @@ public class PersonService {
     private final VisibilityStatusService visibilityStatusService;
     private final ModelMapper modelMapper;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final FriendshipRepository friendshipRepository;
+    private final FriendshipStatusService friendshipStatusService;
 
     public Person getPersonByUuid(String uuid) {
         return personRepository.findById(uuid).orElse(null);
@@ -84,9 +79,29 @@ public class PersonService {
                         visibilityStatusService.getById(VisibilityStatus.VISIBLE)
                 );
         // TODO: 9/25/2022 pridat funkcionalitu, aby vracel pouze lidi, ktere jeste nema v friendship, pokud ma tak pouze REJECTED status  
+
+        List<Friendship> friendships =
+                friendshipRepository.findFriendshipsByMainPersonOrPersonAndStatusNot(
+                        person,
+                        person,
+                        friendshipStatusService.getById(FriendshipStatus.REJECTED)
+                );
+
         people = people
                 .stream()
                 .filter(p -> !p.getUuid().equals(personUuid))
+                .filter(p ->
+                        friendships
+                                .stream()
+                                .filter(f -> {
+                                    if (f.getPerson().getUuid() == p.getUuid() || f.getMainPerson().getUuid() == p.getUuid()) {
+                                        return false;
+                                    }
+                                    return true;
+                                })
+                                .collect(Collectors.toList())
+                                .isEmpty()
+                )
                 .collect(Collectors.toList());
 
         return new ServiceResponse<>(people, "Available people", ServiceResponse.OK);
