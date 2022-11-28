bb2a2af372e4f2c7055c1c04e861507a7d0ff008
Dominik Vyroubal <86168529+Werfmon@users.noreply.github.com>
2022-11-28 12:16:45 +0100

    Update ChatAppV2Application.java

--- src/main/java/cz/domin/chatappv2/ChatAppV2Application.java
+++ src/main/java/cz/domin/chatappv2/ChatAppV2Application.java
@@ -2,6 +2,10 @@ package cz.domin.chatappv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ChatAppV2Application {
