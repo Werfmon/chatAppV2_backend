dea7ef5e7c0131f07fe3a846b3a9afdbd9692898
Werfmon <d.vyroubal.w@gmail.com>
2022-12-14 22:46:50 +0100

    Added self-deploy to pipeline

--- self-deploy.sh
+++ self-deploy.sh
@@ -1,4 +1,5 @@
-#! /bin/bash.sh 
+#! /bin/bash.sh
+docker-compose down
 git pull origin master
-ENV=prod docker-compose build
-docker-compose up -d
+ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose build
+ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose up -d
