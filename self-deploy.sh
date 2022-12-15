bc6ba327a180c3688419eca2d875ca2980c74e48
Werfmon <d.vyroubal.w@gmail.com>
2022-12-15 15:43:31 +0100

    Added .dockerignore

--- self-deploy.sh
+++ self-deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash.sh
 docker-compose down
+git reset --hard
 git pull origin master
 ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose build
 ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose up -d
