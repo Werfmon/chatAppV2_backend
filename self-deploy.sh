e833d272f3006812be0efca862641911296fe2bf
Werfmon <d.vyroubal.w@gmail.com>
2022-12-16 09:59:45 +0100

    Fix

--- self-deploy.sh
+++ self-deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash.sh
 docker-compose down
-git reset --hard
+git fetch && git reset --hard origin/master
 git pull origin master
 ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose build
 ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose up -d
