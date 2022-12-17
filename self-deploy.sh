2e14cb8af8ab0bb3b72c85557092a1bd370cfcd9
Werfmon <d.vyroubal.w@gmail.com>
2022-12-17 01:06:45 +0100

    Edited self deploy

--- self-deploy.sh
+++ self-deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash.sh
-docker-compose down
+docker-compose down chatapp_spring
 git fetch && git reset --hard origin/master
 git pull origin master
 ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose build
-ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose up -d
+ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose up -d chatapp_spring
