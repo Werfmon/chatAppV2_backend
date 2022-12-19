451bd1e6ad106e4e16b10af0dc9bf32d41aeef5d
Werfmon <d.vyroubal.w@gmail.com>
2022-12-19 16:34:02 +0100

    Last message EP SQL fix

--- self-deploy.sh
+++ self-deploy.sh
@@ -1,3 +1,4 @@
 #! /bin/bash.sh
 git fetch && git reset --hard origin/master
-ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose up -d spring --build
+ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose build spring
+ENV=prod PASSWORD=4fsd5fsd ROOT_PASSWORD=f4s65f4sd docker-compose up -d spring
--- src/main/java/cz/domin/chatappv2/Repository/MessageRepository.java
+++ src/main/java/cz/domin/chatappv2/Repository/MessageRepository.java
@@ -14,7 +14,7 @@ public interface MessageRepository extends JpaRepository<Message, String> {
     @Query(value = "SELECT * FROM message m LEFT JOIN chat c ON m.chat_uuid = c.uuid WHERE c.uuid = :chatUuid ORDER BY m.sent_date DESC LIMIT :offset, :limit", nativeQuery = true)
     List<Message> findMessagesByChat(String chatUuid, Integer limit, Integer offset);
 
-    @Query(value = "SELECT * FROM Message m  WHERE m.chat_uuid = :chatUuid ORDER BY m.sent_date DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM message m  WHERE m.chat_uuid = :chatUuid ORDER BY m.sent_date DESC LIMIT 1", nativeQuery = true)
     Optional<Message> findTopByChat_Uuid(String chatUuid);
 
     List<Message> findMessagesByChat_UuidAndAndSeenAndPersonIsNot(String chatUuid, Boolean seen, Person person);
